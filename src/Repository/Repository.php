cb317886f74eaad95d1d9893d88cd30c4d55a0d8
DoryanPr <67996796+DoryanPr@users.noreply.github.com>
2020-10-21 09:17:25 +0200

    a

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php 
 /**
  * @author Doryan
@@ -19,29 +18,4 @@ abstract class Repository
         $this->classname = $classname;
         $this->table = strtolower($classname);
     }
-=======
-<?php 
-/**
- * @author Doryan
- * date 19/10/20
- */
-
-use App\Config\DbConfig;
-
-abstract class Repository
-{
-    protected PDO $pdo;
-    private string $table;
-    private string $classname;
-
-    public function __construct(string $classname)
-    {
-        $this->pdo = new PDO(DbConfig::DSN, DbConfig::USERNAME, DbConfig::PASSWORD);
-        $this->classname = $classname;
-        $this->table = strtolower($classname);
-    }
-
-
-    
->>>>>>> Stashed changes
 }
