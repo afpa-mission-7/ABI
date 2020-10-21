ad311935133006a0d43fafc82a4a1d7c04b964e4
Aymeric-Mary <aymeric.mary.pls@gmail.com>
2020-10-21 08:26:08 +0200

    m

--- src/Forms/loginForm.php
+++ src/Forms/loginForm.php
@@ -1,40 +1,37 @@
-<?php
-
+<?php 
 namespace App\Forms;
-
 use \PDO;
 use App\Config\DbConfig;
+
+
 
 
 class LoginForm
 {
 
-    private string $username;
     private string $password;
+    private string $email;
 
-    public function __construct($post)
-    {
-        $this->username = $post['username'];
-        $this->password = $post['password'];
+    public function __construct($post){
+        $this->email=$post['email'];
+        $this->password=$post['password'];
+
+
     }
 
-    public function login(): bool
-    {
+    public function login(){
         $pdo = new PDO(DbConfig::DSN, DbConfig::USERNAME, DbConfig::PASSWORD);
 
         // Récupération de l'utilisateur et de son PASSWORD
-        $query = $pdo->prepare('SELECT * FROM collaborator WHERE username = ?');
-        $query->execute([$this->username]);
-        $query->setFetchMode(PDO::FETCH_CLASS, 'App\Entity\Collaborator');
-        $collaborator = $query->fetch();
-        if (!$collaborator) return false;
-        // Comparaison du pass envoyé via le formulaire avec la BDD
+        $req = $pdo->prepare('SELECT login .* FROM login WHERE email = :email');
+        $req->execute([':email'=> $this->email]);
+        $resultat = $req->fetch();
 
-        if ($collaborator->getPassword() === $this->password) {
-            $_SESSION['id'] = $collaborator->getId();
-            return true;
-        } else {
-            return false;
-        }
+        // Comparaison du pass envoyé via le formulaire avec la BDD
+       if($resultat['password'] === $this->password ){
+            $_SESSION['name'] = $resultat['name'];
+       } else {
+           dd('Raté');
+       }
     }
 }
