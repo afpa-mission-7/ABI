68a5e0c04d6f2c80d3bb39ec9144bca93075511e
Simon <simonbascobert@gmail.com>
2020-10-26 10:58:09 +0100

    s

--- templates/gestionclients.php
+++ templates/gestionclients.php
@@ -32,10 +32,10 @@
   <tbody>
     <?php foreach ($customers as $customer) : ?>
       <tr id="<?= $customer->getId() ?>" class="row_customer">
-        <td scope="row"><?= $customer->getCompanyName() ?></th>
-        <td><?= $customer->getZip() ?></td>
-        <td><?= $customer->getCity() ?></td>
-        <td><?= $customer->getPhone() ?></td>
+        <td scope="row" class="company_name"><?= $customer->getCompanyName() ?></th>
+        <td class="zip"><?= $customer->getZip() ?></td>
+        <td class="city"><?= $customer->getCity() ?></td>
+        <td class="phone"><?= $customer->getPhone() ?></td>
       </tr>
     <?php endforeach; ?>
   </tbody>
